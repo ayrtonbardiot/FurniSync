446b1fd9de33ce0ad7c2c1f19db471519ea878de
notaryzw3b <modz.ayrton@gmail.com>
2020-10-26 14:17:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var config = require('config');
+var mysql = require('mysql');
+var emulator = config.get('app.emulator')
+var query;
+var db = mysql.createConnection({
+    host: config.get('mysql.host'),
+    user: config.get('mysql.username'),
+    password: config.get('mysql.password'),
+    database: config.get('mysql.database')
+})
+
+db.connect();
+
+switch (emulator) {
+    case "plus":
+        query = "SELECT public_name FROM furniture";
+        break;
+    case "comet":
+        query = "QUERY";
+        break;
+    case "arcturus":
+        query = "SELECT public_name FROM items_base";
+        break;    
+}
+db.query(query, function (error, results, fields) {
+    if (error) throw error;
+    for (var i = 0; i < results.length; i++) {
+        console.log(results[i].public_name);
+    }
+    if (i === results.length) {
+        console.log('Finsished !')
+        process.exit();
+    }
+})
+
